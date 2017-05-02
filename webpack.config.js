9f94427d24ec5b394bbf1a1c9bc6f99d20e15167
DarQ522 <kreatywny95@gmail.com>
2017-05-02 19:47:14 +0200

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+var debug = process.env.NODE_ENV !== "production";
+var webpack = require('webpack');
+
+module.exports = {
+  context: __dirname,
+  devtool: debug ? "inline-sourcemap" : null,
+  entry: "./src/index.js",
+  output: {
+    path: __dirname,
+    filename: "bundle.js"
+  },
+  plugins: debug ? [] : [
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
+  ],
+  module: {
+    loaders: [
+      { test: /\.css$/, loader: 'style-loader!css-loader' }
+    ]
+  }
+};
+
+
+
+
